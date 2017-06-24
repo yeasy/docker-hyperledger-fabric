159d761779d1ada2805384893b911801fb9b56ce
Baohua Yang <yangbaohua@gmail.com>
2017-06-24 09:08:43 +0800

    Update crypto-config and scripts

---
+++ v1.0.0-rc1/scripts/orderer_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+echo "Building fabric orderer"
+CGO_CFLAGS=" " \
+go install -tags "" \
+-ldflags "-X github.com/hyperledger/fabric/common/metadata.Version=1.0.0-preview \
+            -X github.com/hyperledger/fabric/common/metadata.BaseVersion=0.3.1 \
+            -X github.com/hyperledger/fabric/common/metadata.BaseDockerLabel=org.hyperledger.fabric \
+            -X github.com/hyperledger/fabric/common/metadata.DockerNamespace=hyperledger \
+            -X github.com/hyperledger/fabric/common/metadata.BaseDockerNamespace=hyperledger" \
+github.com/hyperledger/fabric/orderer
