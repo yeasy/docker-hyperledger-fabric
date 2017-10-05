67e5127317becdbd1fe4e8a11af9c7945d81d8f5
Baohua Yang <yangbaohua@gmail.com>
2017-10-05 16:44:32 +0800

    Remove bin before building to avoid misusage

--- scripts/orderer_build.sh
+++ scripts/orderer_build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
+set -e
+
+ORDERER_BIN=`which orderer`
+
+echo "Remove $ORDERER_BIN"
+rm -f $ORDERER_BIN
+
 echo "Building fabric orderer"
 cd $FABRIC_ROOT/orderer \
     && CGO_CFLAGS=" " go install -ldflags "$LD_FLAGS -linkmode external -extldflags '-static -lpthread'" \
--- scripts/peer_build.sh
+++ scripts/peer_build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
 
-echo "Building fabric peer"
+set -e
 
+PEER_BIN=`which peer`
+
+echo "Remove $PEER_BIN"
+rm -f $PEER_BIN
+
+echo "Building fabric peer"
 cd $FABRIC_ROOT/peer \
     && CGO_CFLAGS=" " go install -ldflags "$LD_FLAGS -linkmode external -extldflags '-static -lpthread'" \
     && go clean 
